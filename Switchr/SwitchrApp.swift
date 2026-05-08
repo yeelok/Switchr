187dd265634ba1d40ec5467d890438542c48312d
yeelok <yeelok@live.hk>
2026-05-08 18:52:05 +0900

    Initial Commit

---
+++ Switchr/SwitchrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchrApp.swift
+//  Switchr
+//
+//  Created by Yee Lok on 2026/05/08.
+//
+
+import SwiftUI
+
+@main
+struct SwitchrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
